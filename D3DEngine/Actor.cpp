c1b311fce457c769faa3adc2e11fb07b0543abd5
Tiancai Ye <tiancai.ye@outlook.com>
2015-04-27 21:36:48 +0800

    component based

--- D3DEngine/Actor.cpp
+++ D3DEngine/Actor.cpp
@@ -4,36 +4,71 @@
 
 Actor* Actor::Create()
 {
-	return NULL;
+	Actor* actor = new Actor();
+	actor->AutoRelease();
+	if (!actor->Init())
+		return NULL;
+	return actor;
 }
 
 bool Actor::Init()
 {
+	transform = Transform::Create();
+	transform->Retain();
 	return true;
 }
 
 void Actor::Cleanup()
 {
 	Node::Cleanup();
+	transform->Release();
 }
 
 void Actor::Update()
 {
 	Node::Update();
 
-	D3DXMATRIX rx, ry, rz, scale, move;
-	D3DXMatrixRotationX(&rx, m_rotation.x);
-	D3DXMatrixRotationY(&ry, m_rotation.y);
-	D3DXMatrixRotationZ(&rz, m_rotation.z);
-	D3DXMatrixScaling(&scale, m_scale.x, m_scale.y, m_scale.z);
-	D3DXMatrixTranslation(&move, m_position.x, m_position.y, m_position.z);
-
-	m_tsworld = scale * rx * ry * rz * move;
+	transform->OnUpdate();
 }
 
 void Actor::Render()
 {
 	Node::Render();
+	
+	transform->OnRender();
+}
+
+Transform* Transform::Create()
+{
+	Transform* tf = new Transform();
+	tf->AutoRelease();
+	if (!tf->Init())
+		return NULL;
+	return tf;
+}
+
+bool Transform::Init()
+{
+	position = D3DXVECTOR3(.0f, .0f, .0f);
+	rotation = D3DXVECTOR3(.0f, .0f, .0f);
+	scale = D3DXVECTOR3(1.0f, 1.0f, 1.0f);
+	return true;
+}
+
+void Transform::OnUpdate()
+{
+
+}
+
+void Transform::OnRender()
+{
+	D3DXMATRIX rx, ry, rz, scaling, move;
+	D3DXMatrixRotationX(&rx, rotation.x);
+	D3DXMatrixRotationY(&ry, rotation.y);
+	D3DXMatrixRotationZ(&rz, rotation.z);
+	D3DXMatrixScaling(&scaling, scale.x, scale.y, scale.z);
+	D3DXMatrixTranslation(&move, position.x, position.y, position.z);
+	m_tsworld = scaling * rx * ry * rz * move;
 
 	Renderer::GetInstance()->SetTransform(D3DTS_WORLD, &m_tsworld);
 }
--- D3DEngine/Actor.h
+++ D3DEngine/Actor.h
@@ -6,15 +6,33 @@ class Actor : public Node
 public:
 	static Actor* Create();
 	bool Init();
+
 	void Cleanup() override;
-	
 	void Update() override;
 	void Render() override;
 
-protected:
-	D3DXVECTOR3 m_position;
-	D3DXVECTOR3 m_rotation;
-	D3DXVECTOR3 m_scale;
+public:
+	Transform* transform;
+};
 
+class Component : public Ref
+{
+public:
+	virtual void OnUpdate() = 0;
+	virtual void OnRender() = 0;
+};
+
+class Transform : public Component
+{
+public:
+	static Transform* Create();
+	bool Init();
+	void OnUpdate() override;
+	void OnRender() override;
+public:
+	D3DXVECTOR3 position;
+	D3DXVECTOR3 rotation;
+	D3DXVECTOR3 scale;
+private:
 	D3DXMATRIX m_tsworld;
 };
